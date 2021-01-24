4712df90d9114cef67f772161b3e093e522498b1
tzznos <acekun141@gmail.com>
2021-01-24 17:54:35 +0700

    initilize

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathWebpackPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ['./src/index.tsx'],
+    vendor: ['react', 'react-dom']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    plugins: [
+      new TsconfigPathWebpackPlugin({ baseUrl: '/src' })
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    open: true
+  }
+}
